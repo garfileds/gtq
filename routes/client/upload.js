25df727533b921f4086ee514700db45f74d809ec
adoug <yuqingya101@163.com>
2016-05-21 18:42:58 +0800

    upload module

--- routes/client/upload.js
+++ routes/client/upload.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('client/upload', { title: 'Express' });
 });
 
 module.exports = router;

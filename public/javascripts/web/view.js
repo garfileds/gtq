7dae94a341b7af58b5b7d881a111a2ede26b2cc2
adoug <yuqingya101@163.com>
2016-05-22 17:42:50 +0800

    web module

--- public/javascripts/web/view.js
+++ public/javascripts/web/view.js
@@ -1,30 +1,71 @@
-var points = [new BMap.Point(116.41413701159672, 39.90795884517671),
-    new BMap.Point(116.3786889372559, 39.90762965106183),
-    new BMap.Point(116.38632786853032, 39.90795884517671),
-    new BMap.Point(116.39534009082035, 39.907432133833574),
-    new BMap.Point(116.40624058825688, 39.90789300648029)
-];
-
-//地图初始化
-var bm = new BMap.Map("allmap");
-bm.centerAndZoom(new BMap.Point(116.378688937, 39.9076296510), 15);
-
-//坐标转换完之后的回调函数
-translateCallback = function(data) {
-    if (data.status === 0) {
-        for (var i = 0; i < data.points.length; i++) {
-            bm.addOverlay(new BMap.Marker(data.points[i]));
-            bm.setCenter(data.points[i]);
+(function() {
+    var url = '/location/get/user1'
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onreadystatechange = function() {
+        if (request.readystate === 4 && request.status === 200) {
+            renderBMap(request.response);
         }
-        bm.enableScrollWheelZoom(true);
+    };
+
+    /*
+     * @fn 处理location数据，调用百度地图显示location
+     */
+    function renderBMap(locations) {
+        if (locations.code === 0) {
+            var len = locations.length,
+                sumX = 0,
+                sumY = 0,
+                points = [];
+
+            sum.map(function(el) {
+                points.push(new BMap.Point(el.x, el.y));
+                sumX += el.x;
+                sumY += el.y;
+            });
+
+            if (points.length === 0) {
+                points = [new BMap.Point(116.41413701159672, 39.90795884517671),
+                    new BMap.Point(116.3786889372559, 39.90762965106183),
+                    new BMap.Point(116.38632786853032, 39.90795884517671),
+                    new BMap.Point(116.39534009082035, 39.907432133833574),
+                    new BMap.Point(116.40624058825688, 39.90789300648029)
+                ];
+            }
+
+            //地图初始化
+            var bm = new BMap.Map("allmap");
+            bm.centerAndZoom(new BMap.Point(sumX / len, sumY / len), 15);
+
+            //坐标转换完之后的回调函数
+            var translateCallback = function(data) {
+                if (data.status === 0) {
+                    var i, maker;
+
+                    for (i = 0; i < data.points.length; i++) {
+                        (function() {
+                            var imgId = locations[i].time.getTime();
+                            var infoWindow = '<img src="' + locations[i].img + ' style="width: 150px;height: 150px">';
+                            function showImg() {
+
+                            }
+                        })();
+
+                        maker = new BMap.Marker(data.points[i]);
+                        maker.addEventListener('click', showImgGeneration());
+                        bm.addOverlay(maker);
+                        bm.setCenter(data.points[i]);
+                    }
+                    bm.enableScrollWheelZoom(true);
+                }
+            };
+
+            setTimeout(function() {
+                var convertor = new BMap.Convertor();
+                convertor.translate(points, 1, 5, translateCallback);
+            }, 1000);
+        } else {
+            alert(data.desc);
+        }
     }
-};
-
-setTimeout(function() {
-    var convertor = new BMap.Convertor();
-    convertor.translate(points, 1, 5, translateCallback);
-}, 1000);
-
-var url = '/web/view'
-var request = new XMLHttpRequest();
-request.open('GET', url);
+})();
--- routes/client/upload.js
+++ routes/client/upload.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('client/upload', { title: 'Express' });
+  res.render('client/upload', { title: 'GTQ系统-图片上传' });
 });
 
 module.exports = router;

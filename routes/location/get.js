2de328076913bcb69c0b0fbe85500fca39bf68ec
adoug <yuqingya101@163.com>
2016-06-19 10:37:20 +0800

    fix

--- routes/location/get.js
+++ routes/location/get.js
@@ -9,10 +9,12 @@ router.get('/:id', function(req, res) {
     var Location = global.dbHandel.getModel('location');
 
     /*test code*/
-    /*req.session.user = {
-        _id: '573fd5db00adc2f02f15701d',
-        code: 'user1'
-    };*/
+    if (req.session.user.code != 'admin') {
+        req.session.user = {
+            _id: '573fd5db00adc2f02f15701d',
+            code: 'user1'
+        };
+    }
     /*test code end*/
     if (req.session.user.code === 'admin' || req.session.user.code === req.params.id) {
         Location.find({

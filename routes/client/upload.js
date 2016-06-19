c3d6eb9df9cc530986eb0e2b50c5d436b2d89e0e
adoug <yuqingya101@163.com>
2016-06-19 10:14:25 +0800

    fix

--- routes/client/upload.js
+++ routes/client/upload.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	console.log('req.session.user: ' + req.session.user.code);
+	/*console.log('req.session.user: ' + req.session.user.code);
 	if (!req.session.user) {
         req.session.err = '用户未登录';
         res.sendStatus(500);
-    } else {
+    } else {*/
     	res.render('client/upload', {
 	        title: 'GTQ系统-图片上传',
-	        userCode: req.session.user.code
-	            // userCode: 'user1' //test code
+	        // userCode: req.session.user.code
+	        userCode: 'user1' //test code
 	    });
-    }
+    /*}*/
 });
 
 module.exports = router;

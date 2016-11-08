eb3b648963d633338ec205c8df581a3268e95e0a
adoug <yuqingya101@163.com>
2016-11-08 22:35:24 +0800

    access wechat

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('web/login', { title: 'GTQ-登录' });
 });
 
-module.exports = router;
+module.exports = router;*/
+
+router.get('/', function(req, res, next) {
+	/*var signature = req.query.signature,
+		timestamp = req.query.timestamp,
+		nonce = req.query.nonce,
+		echostr = req.query.echostr;
+
+	var sha1, tmpStr;
+	var tmpArr = [signature, timestamp, nonce];
+	tmpArr.sort();
+
+	sha1 = crypto.createHash('sha1');
+	sha1.update(tmpArr.join(''));
+	tmpStr = sha1.digest('hex');
+
+	if (tmpStr === signature) {
+		res.status(200).send(echostr);
+	} else {
+		res.status(500).send('error');
+	}*/
+	var echostr = req.query.echostr;
+	res.status(200).send(echostr);
+});
